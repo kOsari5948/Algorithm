d8e6d949b4cb6d1a184e6494c7294f7d753424f5
MinCheolKim <86839047+kOsari5948@users.noreply.github.com>
2024-11-10 15:54:51 +0900

    20241110
    이번 문제는 단순히 bfs를 통해 찾는 문제였다. 단 중복이 허용이 되야 횟수를 체크할 수 있기 때문에 중복을 허용해주는 코드가 필요하다

---
+++ 20241110/12851.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static int N, K;
+    static int[] time = new int[100001];
+    static int minTime = Integer.MAX_VALUE;
+    static int count = 0;
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        K = sc.nextInt();
+
+        //만약 뒤로 가야하면 차피 순간이동으로는 움직일 수 없음
+        if (N >= K) {
+            System.out.println((N-K) + "\n1");
+            return;
+        }
+
+        bfs();
+
+        System.out.println(minTime + "\n" + count);
+    }
+
+    static void bfs() {
+        Queue<Integer> q = new LinkedList<Integer>();
+
+        q.add(N);
+        time[N] = 1;
+
+        while (!q.isEmpty()) {
+            int now = q.poll();
+
+            // now 방문 시간이 최소 시간보다 크면 뒤는 더 볼 필요 없음
+            if (minTime < time[now]) return;
+
+            for (int i=0; i<3; i++) {
+                int next;
+
+                if (i == 0)         next = now + 1;
+                else if (i == 1)    next = now - 1;
+                else                next = now * 2;
+
+                if (next < 0 || next > 100000) continue;
+
+                if (next == K) {
+                    minTime = time[now];
+                    count++;
+                }
+
+                // 첫 방문이거나 (time[next] == 0)
+                // 이미 방문한 곳이어도 같은 시간에 방문했다면 (time[next] == time[now] + 1)
+                // 경우의 수에 추가될 수 있기 때문에 Queue 에 한번 더 넣어줌
+                if (time[next] == 0 || time[next] == time[now] + 1) {
+                    q.add(next);
+                    time[next] = time[now] + 1;
+                }
+            }
+        }
+    }
+}
