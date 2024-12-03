0932a5874b041da3610c1c2269997f657bf20625
MinCheolKim <86839047+kOsari5948@users.noreply.github.com>
2024-12-03 11:33:16 +0900

    20241203
    20241203일자 알고리즘 공부 이번 문제는 단순한 유니온 파인드 문제였다.

---
+++ 20241203/200040.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static int n, m ;
+	static int[] parent;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		parent = new int[n];
+		for(int i=0; i<n; i++) {
+			parent[i] = i;
+		}
+		
+		for(int i =0; i<m;i++) {
+			st = new StringTokenizer(br.readLine());
+			int x =  Integer.parseInt(st.nextToken());
+			int y =  Integer.parseInt(st.nextToken());
+			
+			if(find(x)==find(y)) {
+				System.out.println(i+1);
+				return;
+			}else {
+				union(x,y);
+			}
+		}
+		
+		System.out.println(0);
+		
+		
+	}
+	
+	static int find(int x) {
+		if(x == parent[x]) {
+			return x;
+		}
+		return parent[x] = find(parent[x]);
+	}
+	
+	static void union(int x, int y) {
+		x = find(x);
+		y = find(y);
+		
+		if(x < y) {
+			parent[y] = x;
+		}else {
+			parent[x] = y;
+		}
+	}
+		
+}
